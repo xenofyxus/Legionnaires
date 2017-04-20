9d1ead50b1031a6b24dd0f9e091916ab7065f6e3
VirreCarre <victor.carle@gmail.com>
2017-04-20 16:06:40 +0200

    Project Created

---
+++ Assets/Scripts/MainMenuBTN.cs
@@ -0,0 +1,18 @@
+﻿//Author: Victor och Daniel
+//Date: 20/04/2017
+//Edited by:
+//Edit date:
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBTN : MonoBehaviour {
+
+	public void StartLevel() {
+		SceneManager.LoadScene (1); //This decides which Scene will load when the play button is pressed.
+	}
+
+}
