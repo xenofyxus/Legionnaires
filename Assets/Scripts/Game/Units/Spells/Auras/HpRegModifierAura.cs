fb3e28e5e23e18a7f5a3550b337ea68a2404fbe5
Alexander Krantz <klako@hotmail.se>
2017-04-24 17:04:13 +0200

    Added Spells

---
+++ Assets/Scripts/Game/Units/Spells/Auras/HpRegModifierAura.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Game.Units.Spells.Auras
+{
+    public class HpRegModifierAura:Aura
+    {
+        public float hpRegModifier = 0f;
+
+        Dictionary<UnitBehaviour, float> hpRegDeltas=new Dictionary<UnitBehaviour, float>();
+
+        protected override void Applying(UnitBehaviour unit)
+        {
+            float newHpReg = unit.hpReg + hpRegModifier;
+            hpRegDeltas.Add(unit, newHpReg - hpRegModifier);
+            unit.hpReg = newHpReg;
+        }
+
+        protected override void Removing(UnitBehaviour unit)
+        {
+            float hpRegDelta;
+            if(hpRegDeltas.TryGetValue(unit, out hpRegDelta))
+            {
+                unit.hpReg -= hpRegDelta;
+            }
+        }
+    }
+}
+
