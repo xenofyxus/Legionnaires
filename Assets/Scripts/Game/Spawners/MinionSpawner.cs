0fb1887c473e12372c4e87c92b275a97eb9a0d1b
Daniel <b.daniel.karlsson@gmail.com>
2017-04-25 08:44:43 +0200

    Waves can contain different gameobj

--- Assets/Scripts/Game/Spawners/MinionSpawner.cs
+++ Assets/Scripts/Game/Spawners/MinionSpawner.cs
@@ -14,7 +14,14 @@ using UnityEngine;
 
 public class MinionSpawner : MonoBehaviour {
 
-	public GameObject minionToSpawn;
+	public GameObject minionType1;
+	public GameObject minionType2;
+	public GameObject minionType3;
+
+	private int waveNumber = 0;
+	private GameObject[] waveObjList = new GameObject[3]; 
+
+
 	public GameObject legionnaireSpawner;
 	private GameObject[] waveObjects;
 
@@ -29,6 +36,9 @@ public class MinionSpawner : MonoBehaviour {
 	private float MaxY = 17;
 
 	void Start () {
+		waveObjList [0] = minionType1;
+		waveObjList [1] = minionType2;
+		waveObjList [2] = minionType3;
 	}
 
 	void Update () {
@@ -49,11 +59,15 @@ public class MinionSpawner : MonoBehaviour {
 			for (int i = 0; i < 4; i++) {
 				float x = Random.Range (MinX, MaxX);
 				float y = Random.Range (MinY, MaxY);
-				Instantiate (minionToSpawn, new Vector2 (x, y), transform.rotation);
+				Instantiate (waveObjList[waveNumber], new Vector2 (x, y), transform.rotation);
 			}
 		}
 		if(waveObjects.Length > numberOfUnits){
 			instantiatetimer = 10f;
+			waveNumber++;
+			if (waveNumber == waveObjList.Length) {
+				waveNumber = 0;
+			}
 			newWave = false;
 		}
 	}
