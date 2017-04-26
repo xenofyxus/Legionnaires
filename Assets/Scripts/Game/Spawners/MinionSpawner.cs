448894514232634d8ff51a2b0bd24bdb4bcbad68
VirreCarre <victor.carle@gmail.com>
2017-04-26 10:53:24 +0200

    Dynamic waves, fixed minion prefabs and scene names

--- Assets/Scripts/Game/Spawners/MinionSpawner.cs
+++ Assets/Scripts/Game/Spawners/MinionSpawner.cs
@@ -4,73 +4,81 @@
 /// summary>
 
 //Author: Daniel Karlsson, Victor Carle
-//Updates:
 //Date: 24/4-17
-
+//Updates: Anton Anderzen, Victor Carle
+//Added dynamic wave count and minion amount/wave.
+//Date: 26/4-17
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MinionSpawner : MonoBehaviour {
-
-	public GameObject minionType1;
-	public GameObject minionType2;
-	public GameObject minionType3;
-
+public class MinionSpawner : MonoBehaviour
+{
+	
 	private int waveNumber = 0;
-	private GameObject[] waveObjList = new GameObject[3]; 
-
+	[Tooltip ("What minion and how many of that minion to Spawn")]
+	public List<Game.Spawners.WaveObject> waveObjList = new List<Game.Spawners.WaveObject> ();
 
 	public GameObject legionnaireSpawner;
 	private GameObject[] waveObjects;
 
 	private bool newWave;
-	private int numberOfUnitsInWave = 25;
 	private int numberOfUnitsSpawned = 0;
-	private float instantiatetimer = 5f; //time until next wave starts.
+	private float instantiateTimer = 5f;
+	//time until next wave starts.
 
 	private float MinX = -3;
 	private float MaxX = 3;
 	private float MinY = 16;
 	private float MaxY = 17;
 
-	void Start () {
-		waveObjList [0] = minionType1;
-		waveObjList [1] = minionType2;
-		waveObjList [2] = minionType3;
+	void Start ()
+	{
 	}
 
-	void Update () {
+	void Update ()
+	{
 		waveObjects = GameObject.FindGameObjectsWithTag ("Minion");
 		if (waveObjects.Length == 0) {
 			legionnaireSpawner.GetComponent<LegionnaireSpawner> ().Reset ();
-
 			newWave = true;
 		}
 		if (newWave) {
 			NextWave ();
 		}
 	}
-	void NextWave(){
-		instantiatetimer -= Time.deltaTime;
-		if (instantiatetimer <= 0) {
-			instantiatetimer = 1f;
-			for (int i = 0; i < 4; i++) {
-				float x = Random.Range (MinX, MaxX);
-				float y = Random.Range (MinY, MaxY);
-				Instantiate (waveObjList[waveNumber], new Vector2 (x, y), transform.rotation);
-				numberOfUnitsSpawned++;
+
+
+	void NextWave ()
+	{
+
+		for (int i = 0; i < Mathf.Ceil (((float)waveObjList [waveNumber].amountOfMinions) / 4); i++) {
+			instantiateTimer -= Time.deltaTime;
+			if (instantiateTimer <= 0) {
+				instantiateTimer = 2.5f;
+				for (int j = 0; j < 4; j++) {		// Spawning 4 units per "instantiateTimer"-delay
+					if (numberOfUnitsSpawned >= waveObjList [waveNumber].amountOfMinions) {
+						break;
+					}
+					float x = Random.Range (MinX, MaxX);
+					float y = Random.Range (MinY, MaxY);
+					Instantiate (waveObjList [waveNumber].minion, new Vector2 (x, y), transform.rotation);
+					numberOfUnitsSpawned++;
+
+				}
+
 			}
 		}
-		if(numberOfUnitsSpawned > numberOfUnitsInWave){
-			instantiatetimer = 5f;
+		if (numberOfUnitsSpawned >= waveObjList [waveNumber].amountOfMinions) { // When done spawning goes on to next wave.
+			instantiateTimer = 5f;
 			waveNumber++;
 			numberOfUnitsSpawned = 0;
-			if (waveNumber == waveObjList.Length) {
+			if (waveNumber == waveObjList.Count) {
 				waveNumber = 0;
 			}
 			newWave = false;
 		}
 	}
 }
+
