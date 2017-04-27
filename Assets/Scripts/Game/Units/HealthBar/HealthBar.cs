b30b03676a5400c7a80ffddc178a7af4a10873dd
Daniel <b.daniel.karlsson@gmail.com>
2017-04-27 12:54:13 +0200

    Added Healthbars as prefabs

---
+++ Assets/Scripts/Game/Units/HealthBar/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Interface.Units
+{
+
+	public class HealthBar : MonoBehaviour {
+
+		Quaternion rotation;
+		public Image HealthBars;
+		Game.Units.UnitBehaviour unit;
+	
+		void Awake()
+		{
+			unit = transform.root.GetComponent<Game.Units.UnitBehaviour> ();
+			rotation = transform.rotation;
+		}
+		void LateUpdate()
+		{
+			HealthBars.fillAmount = unit.hp / unit.hpMax;
+			transform.rotation = rotation;
+		}
+	}
+}
---
+++ Assets/Scripts/Game/Units/HealthBar/NoRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoRotation : MonoBehaviour {
+	Quaternion rotation;
+
+
+	void Awake()
+	{
+		rotation = transform.rotation;
+	}
+	void LateUpdate()
+	{
+		transform.rotation = rotation;
+	}
+}
