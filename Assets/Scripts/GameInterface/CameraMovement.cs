21dc3b6a29d59ded084b0488024bbbb5740d3946
Alexander Krantz <klako@hotmail.se>
2017-05-02 14:51:45 +0200

    Fixed compilation errors

--- Assets/Scripts/GameInterface/CameraMovement.cs
+++ Assets/Scripts/GameInterface/CameraMovement.cs
@@ -12,10 +12,10 @@ public class CameraMovement : MonoBehaviour {
 	void LateUpdate()
 	{
 		
-		if (Game.Units.LegionnaireBehaviour.legionnaires.Count == 0 && Game.Units.MinionBehaviour.minions.Count != 0) {
+		if (Game.Units.LegionnaireBehaviour.legionnaires.Count == 0 && Game.Units.MinionBehaviour.Minions.Count != 0) {
 
-			if (Game.Units.MinionBehaviour.minions [0].transform.position.y < Camera.main.transform.position.y && Camera.main.transform.position.y > -7.3) {
-				Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, new Vector3 (0, Game.Units.MinionBehaviour.minions [0].transform.position.y, -10), 3 * Time.deltaTime);
+			if (Game.Units.MinionBehaviour.Minions [0].transform.position.y < Camera.main.transform.position.y && Camera.main.transform.position.y > -7.3) {
+				Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, new Vector3 (0, Game.Units.MinionBehaviour.Minions [0].transform.position.y, -10), 3 * Time.deltaTime);
 			}
 		}
 	}
