289d15a63907ad37c5e9815296c7848bfde196cd
Alexander Krantz <klako@hotmail.se>
2017-05-02 14:34:48 +0200

    Moved HealthBar scripts, Added namespaces

---
+++ Assets/Scripts/Game/Units/Interface/HealthBar/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Game.Units.Interface.HealthBar
+{
+    public class HealthBar : MonoBehaviour
+    {
+        Quaternion rotation;
+        public Image HealthBars;
+        Game.Units.UnitBehaviour unit;
+
+        void Awake()
+        {
+            unit = transform.root.GetComponent<Game.Units.UnitBehaviour>();
+            rotation = transform.rotation;
+        }
+
+        void LateUpdate()
+        {
+            HealthBars.fillAmount = unit.Hp / unit.HpMax;
+            transform.rotation = rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Units/Interface/HealthBar/NoRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Units.Interface.HealthBar
+{
+    public class NoRotation : MonoBehaviour
+    {
+        Quaternion rotation;
+
+
+        void Awake()
+        {
+            rotation = transform.rotation;
+        }
+
+        void LateUpdate()
+        {
+            transform.rotation = rotation;
+        }
+    }
+
+}
