f05421e6398e84ee4ea5316d916c32e6ca4b486b
NicholasEkroth <nekroth@kth.se>
2017-05-02 11:12:55 +0200

    Changed Interface folder name to GameInterface, added pngs, added finished gameinterface menu and moved around some temporary pngs

---
+++ Assets/Scripts/GameInterface/MinionWaveCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinionWaveCounter : MonoBehaviour {
+
+    private int minionCount;
+    private Text myText;
+
+	void Update()
+	{
+		myText = GetComponent<Text>();
+		int tags = GameObject.FindGameObjectsWithTag("Minion").Length;
+		if(minionCount != tags)
+		{
+			minionCount = tags;
+			myText.text = "Remaining: " + minionCount; 
+		} 
+	}
+}
---
+++ Assets/Scripts/GameInterface/ToggleScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleScript : MonoBehaviour {
+
+	public GameObject current;
+	public Sprite toggleOn;
+	public Sprite toggleOff;
+
+	private bool state = true;
+
+	public void toggleMe()
+	{
+		Image img = current.GetComponent<Image> ();
+		state = !state;
+		if (state) {
+			img.sprite = toggleOn;
+			//Set Sound On.
+		} else {
+			img.sprite = toggleOff;
+			//Set Sound Off.
+		}
+	}
+		
+		
+}
