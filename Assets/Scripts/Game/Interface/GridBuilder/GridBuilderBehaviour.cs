5556f335d9fe3133d5f53bff2a3f8122fa96549f
VirreCarre <victor.carle@gmail.com>
2017-05-04 15:27:44 +0200

    Added upgrades to towers

--- Assets/Scripts/Game/Interface/GridBuilder/GridBuilderBehaviour.cs
+++ Assets/Scripts/Game/Interface/GridBuilder/GridBuilderBehaviour.cs
@@ -6,22 +6,25 @@ namespace Game.Interface.GridBuilder
 {
 	public class GridBuilderBehaviour : MonoBehaviour
 	{
-
+		[System.Serializable]
+		public class towerList{
+			public GameObject[] upgradedTowers;
+		}
 
 		float offSetX = 1.48f;
 		float offSetY = 1.47f;
-		public GameObject[] towersAvailable = new GameObject[6];
+
+		public towerList[] towersAvailable = new towerList[6];
 		public GameObject menu;
 		public GameObject sell;
 		int whichSpotX;
 		int whichSpotY;
 		Vector2 placeTower;
 		GameObject[,] towerGridPos = new GameObject[5, 7];
-		GameObject[,] originalGridPos = new GameObject[5, 7];
+		int[,] originalTower = new int[5, 7];
 		Vector2[,] originalVector = new Vector2[5, 7];
-		List<Vector2> towerPositions = new List<Vector2>();
-		List<GameObject> towersSpawned = new List<GameObject> ();
-		List<GameObject> towersSpawnedNotClone = new List<GameObject> ();
+		int[,] whatUpgrade = new int[5, 7];
+		bool upgradeable;
 		// Use this for initialization
 		void Start ()
 		{
@@ -32,6 +35,7 @@ namespace Game.Interface.GridBuilder
 		void Update ()
 		{
 			if (Input.GetMouseButtonDown (0) && GameObject.FindGameObjectsWithTag ("TowerMenu").Length == 0) {
+				
 				Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 				whichSpotX = Mathf.FloorToInt ((mouseWorldPos.x + 3.8f) / offSetX);
 				whichSpotY = Mathf.FloorToInt (mouseWorldPos.y / offSetY) - 1;
@@ -43,6 +47,7 @@ namespace Game.Interface.GridBuilder
 					Instantiate (sell, placeTower, transform.rotation);
 					TowerMenu.TowerMenuBehaviour.placeTower = false;
 					TowerMenu.TowerMenuBehaviour.sellTower = true;
+					upgradeable = true;
 				}
 			}
 			if (TowerMenu.TowerMenuBehaviour.currentMenuItem != -1 && TowerMenu.TowerMenuBehaviour.placeTower == true) {
@@ -55,31 +60,44 @@ namespace Game.Interface.GridBuilder
 				TowerMenu.TowerMenuBehaviour.currentMenuItem = -1;
 				TowerMenu.TowerMenuBehaviour.sellTower = false;
 			}
+			if (TowerMenu.TowerMenuBehaviour.currentMenuItem == 1 && upgradeable == true) {
+				upgradeable = false;
+				upgradeTower ();
+				TowerMenu.TowerMenuBehaviour.currentMenuItem = -1;
+				TowerMenu.TowerMenuBehaviour.sellTower = false;
+			}
 		}
 
 		void spawnTower ()
 		{
-			towerGridPos [whichSpotX, whichSpotY] = Instantiate (towersAvailable [TowerMenu.TowerMenuBehaviour.currentMenuItem], placeTower, transform.rotation);
-			originalGridPos [whichSpotX, whichSpotY] = towersAvailable [TowerMenu.TowerMenuBehaviour.currentMenuItem];
+			towerGridPos [whichSpotX, whichSpotY] = Instantiate (towersAvailable [TowerMenu.TowerMenuBehaviour.currentMenuItem].upgradedTowers[0], placeTower, transform.rotation);
+			originalTower [whichSpotX, whichSpotY] = TowerMenu.TowerMenuBehaviour.currentMenuItem;
 			originalVector [whichSpotX, whichSpotY] = placeTower;
+			whatUpgrade [whichSpotX, whichSpotY] = 0;
 		}
 
 		void sellTower() 
 		{
 			GameObject.Destroy(towerGridPos [whichSpotX, whichSpotY]);
-			towerGridPos[whichSpotX,whichSpotY] = null;
-			originalGridPos[whichSpotX, whichSpotY] = null;
+			towerGridPos[whichSpotX, whichSpotY] = null;
 		}
 
 		public void Reset (){
 			for (int i = 0; i < towerGridPos.GetLength (0); i++) {
 				for (int j = 0; j < towerGridPos.GetLength (1); j++) {
-					if (originalGridPos [i, j] != null) {
+					if (towerGridPos [i, j] != null) {
 						GameObject.Destroy (towerGridPos [i, j]);
-						towerGridPos [i, j] = Instantiate(originalGridPos[i,j], originalVector[i,j], transform.rotation);
+						towerGridPos [i, j] = Instantiate(towersAvailable[originalTower[i,j]].upgradedTowers[whatUpgrade[i,j]], originalVector[i,j], transform.rotation);
 					}
 				}
 			}
+		}
+		void upgradeTower(){
+			sellTower ();
+			if (2 > whatUpgrade [whichSpotX, whichSpotY]) {
+				whatUpgrade [whichSpotX, whichSpotY] += 1;
+			}
+			towerGridPos [whichSpotX, whichSpotY] = Instantiate (towersAvailable [originalTower[whichSpotX, whichSpotY]].upgradedTowers[whatUpgrade[whichSpotX, whichSpotY]], originalVector [whichSpotX, whichSpotY], transform.rotation);
 		}
 	}
 }
