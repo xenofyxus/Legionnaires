07241bd6c398ec093b14d06af7c423f7d3a01e99
Markus Winroth <vakeneller@gmail.com>
2017-05-05 09:52:09 +0200

    Added shockwave behaviour and scripts

---
+++ Assets/Scripts/Game/Units/Spells/KingSpells/ShockwaveBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System;
+
+namespace Game.Units.Spells.Kingspells
+{
+
+	public class ShockwaveBehaviour : MonoBehaviour
+	{
+
+		GameObject ShockwaveCopy;
+		Vector2 kingPosition = new Vector2 (0.17f, -14.49f);
+		Vector2 direction;
+		Vector2 mousePosition;
+	
+		[SerializeField]
+		public float damage;
+		public float range;
+
+		//When a copy spawns get the mouseposition of the last click to get direction and assign ShockwaveCopy to that object.
+		void Awake(){
+			mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			direction = mousePosition + (100 * (mousePosition - kingPosition));
+			ShockwaveCopy = this.gameObject;
+		}
+
+
+		//Destroy the shockwave travelled outside of range from the king else move towards the mouseclick
+		void FixedUpdate ()
+		{
+			if (ShockwaveCopy != null) {
+				if (Vector2.Distance (kingPosition, ShockwaveCopy.transform.position) > range) {
+					Destroy (ShockwaveCopy);
+				} else {
+					ShockwaveCopy.transform.position = Vector2.MoveTowards (ShockwaveCopy.transform.position, direction, 0.2f);
+				}
+			}
+		}
+
+
+		//Apply damage to minions when they trigger the rigidbody
+		void OnTriggerEnter2D (Collider2D other)
+		{
+			if (other.gameObject.GetComponent<Game.Units.MinionBehaviour> () != null) {
+				other.GetComponent<UnitBehaviour> ().ApplyDamage (damage);
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/Game/Units/Spells/KingSpells/ShockwaveButton.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System;
+
+namespace Game.Units.Spells.Kingspells
+{
+	public class ShockwaveButton : MonoBehaviour
+	{
+		[SerializeField]
+		public float cooldown;
+		private float cooldownTimer;
+
+
+		GameObject ShockwaveCopy;
+		Vector2 mousePosition;
+		Vector2 mousePositionNew;
+		Vector2 kingPosition = new Vector2 (0.17f, -14.49f);
+		bool isClicked = false;
+
+		//Initiate the timer to the cooldown value
+		void Start ()
+		{
+			cooldownTimer = cooldown;
+		}
+
+
+		//Assign mouseposition if cooldowntimer allows us
+		public void onClick ()
+		{
+			if (cooldownTimer <= 0f) {
+				mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				isClicked = true;
+			}
+		}
+
+
+		void FixedUpdate ()
+		{
+			//Count down the timer
+			if (cooldownTimer > 0f) {
+				cooldownTimer -= Time.deltaTime;
+			}
+
+			mousePositionNew = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+
+			//Checks when it's clicked a second time and if so, spawns shockwave copy
+			if (isClicked && Input.GetMouseButtonDown (0) && (mousePosition != mousePositionNew) && cooldownTimer <= 0f) {
+				mousePosition = mousePositionNew;
+
+				ShockwaveCopy = Instantiate (Resources.Load ("ShockwavePrefab"), kingPosition, Quaternion.identity) as GameObject;
+				ShockwaveCopy.transform.rotation = Quaternion.FromToRotation (Vector2.up, mousePositionNew - (Vector2)ShockwaveCopy.transform.position);
+
+				isClicked = false;
+				cooldownTimer = cooldown;
+			} 
+		}
+	}
+}
