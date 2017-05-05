5b4a12bc33373b8a4ec59dc5a9d08514dc36c282
Markus Winroth <vakeneller@gmail.com>
2017-05-05 12:00:40 +0200

    Added stomp implementation and a ground layer

---
+++ Assets/Scripts/Game/Units/Spells/KingSpells/StompBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+using System;
+
+namespace Game.Units.Spells.Kingspells
+{
+
+	public class StompBehaviour : MonoBehaviour
+	{
+
+		GameObject StompCopy;
+	
+		[SerializeField]
+		public float damage;
+		[UnityEngine.Range(0f, 5f)]
+		public float duration;
+
+	
+		//When a copy spawns get the mouseposition of the last click to get direction and assign ShockwaveCopy to that object.
+		void Awake(){
+			StompCopy = this.gameObject;
+		}
+
+
+
+		//Destroy the shockwave travelled outside of range from the king else move towards the mouseclick
+		void FixedUpdate ()
+		{
+			if (StompCopy != null) {
+				StartCoroutine (KillAnimationOnEnd ());
+			}
+					//Destroy (StompCopy);
+					
+			}
+			
+
+		private IEnumerator KillAnimationOnEnd(){
+			yield return new WaitForSeconds(1.0f);
+			Destroy(StompCopy);
+
+
+
+		}
+
+
+		//Apply damage to minions when they trigger the rigidbody
+		void OnTriggerEnter2D (Collider2D other)
+		{
+			if (other.gameObject.GetComponent<Game.Units.MinionBehaviour> () != null) {
+				//other.GetComponent<UnitBehaviour> ().ApplyDamage (damage);
+				Buffs.StunBuff stunBuff = other.GetComponent<UnitBehaviour> ().gameObject.AddComponent<Buffs.StunBuff>();
+				stunBuff.Duration = duration;
+
+	
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/Game/Units/Spells/KingSpells/StompButton.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System;
+
+namespace Game.Units.Spells.Kingspells
+{
+	public class StompButton : MonoBehaviour
+	{
+		[SerializeField]
+		public float cooldown;
+		private float cooldownTimer;
+
+
+		GameObject StompCopy;
+
+		Vector2 kingPosition = new Vector2 (0.17f, -14.49f);
+
+		//Initiate the timer to the cooldown value
+		void Start ()
+		{
+			cooldownTimer = cooldown;
+		}
+
+
+		public void onClick(){
+			if (cooldownTimer <= 0f) {
+
+				StompCopy = Instantiate (Resources.Load ("StompPrefab"), kingPosition, Quaternion.identity) as GameObject;
+
+				cooldownTimer = cooldown;
+			} 
+		}
+
+
+		void FixedUpdate ()
+		{
+			//Count down the timer
+			if (cooldownTimer > 0f) {
+				cooldownTimer -= Time.deltaTime;
+			}
+
+			//Checks when it's clicked a second time and if so, spawns shockwave copy
+
+		}
+	}
+}
