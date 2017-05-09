3ae16d149470e3fa1b8c5eab9ce4b6a87832c829
Alex Lindström <Alex Lindstrm>
2017-05-09 16:09:13 +0200

    Fixed Settings and corrected buildscene

---
+++ Assets/Scripts/Menu/BackToGameBTN.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToGameBTN : MonoBehaviour {
+	GameObject gridFather;
+
+	public void onClick () {
+		gridFather = GameObject.Find ("GameInterface").gameObject.transform.FindChild ("GridFather").gameObject as GameObject;
+		gridFather.SetActive(true);
+	}
+
+}
