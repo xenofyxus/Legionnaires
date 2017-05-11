125e7e36ac94b57fa7388be2dd38b6c6b275afb5
Alexander Krantz <klako@hotmail.se>
2017-05-11 14:04:27 +0200

    Added explode on death spell

---
+++ Assets/Scripts/Game/Units/Spells/Passives/ExplodePassive.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Units.Spells.Passives
+{
+	public class ExplodePassive:Passive
+	{
+		[Header("Spell Data")]
+
+		[SerializeField]
+		private float damage = 10f;
+
+		[SerializeField]
+		private float radius = 1f;
+
+		public float Damage {
+			get {
+				return this.damage;
+			}
+			set {
+				damage = value;
+			}
+		}
+
+		public float Radius {
+			get {
+				return this.radius;
+			}
+			set {
+				radius = value;
+			}
+		}
+
+		protected override void OwnerDied(object sender, EventArgs e)
+		{
+			base.OwnerDied(sender, e);
+			foreach (UnitBehaviour enemy in owner.GetEnemies())
+			{
+				if (Vector2.Distance(owner.transform.position, enemy.transform.position) <= radius)
+				{
+					float dummyVar;
+					enemy.ApplyDamage(damage, out dummyVar, null);
+				}
+			}
+		}
+	}
+}
+
