35ec13eb0da27b8db37837e623e2587622cc7b3c
Alexander Krantz <klako@hotmail.se>
2017-05-11 14:49:07 +0200

    Exposed resource income interval getter

--- Assets/Scripts/Game/Interface/Infobar/Resources/ResourcesBehaviour.cs
+++ Assets/Scripts/Game/Interface/Infobar/Resources/ResourcesBehaviour.cs
@@ -5,154 +5,151 @@ using UnityEngine.UI;
 
 namespace Game.Interface.Infobar.Resources
 {
-    public class ResourcesBehaviour : MonoBehaviour
-    {
-        private static ResourcesBehaviour current;
+	public class ResourcesBehaviour : MonoBehaviour
+	{
+		private static ResourcesBehaviour current;
 
-        public static ResourcesBehaviour Current
-        {
-            get
-            {
-                if(current == null)
-                    current = GameObject.Find("GameInterface/Infobar(Panel)/Resources(Panel)").GetComponent<ResourcesBehaviour>();
-                return current;
-            }
-        }
+		public static ResourcesBehaviour Current {
+			get {
+				if (current == null)
+					current = GameObject.Find("GameInterface/Infobar(Panel)/Resources(Panel)").GetComponent<ResourcesBehaviour>();
+				return current;
+			}
+		}
 
-        [SerializeField]
-        private int gold = 50;
+		[SerializeField]
+		private int gold = 50;
 
-        public int Gold
-        {
-            get{ return gold; }
-            set{ gold = value; }
-        }
+		public int Gold {
+			get{ return gold; }
+			set{ gold = value; }
+		}
 
-        [SerializeField]
-        private int goldIncome = 100;
+		[SerializeField]
+		private int goldIncome = 100;
 
-        public int GoldIncome
-        {
-            get{ return goldIncome; }
-            set{ goldIncome = value; }
-        }
+		public int GoldIncome {
+			get{ return goldIncome; }
+			set{ goldIncome = value; }
+		}
 
 		[SerializeField]
 		private int goldSpent = 0;
 
-		public int GoldSpent
-		{
+		public int GoldSpent {
 			get{ return goldSpent; }
 			set{ goldSpent = value; }
 		}
 
-        [SerializeField]
-        private int wood = 100;
+		[SerializeField]
+		private int wood = 100;
 
-        public int Wood
-        {
-            get { return wood; }
-            set{ wood = value; }
-        }
+		public int Wood {
+			get { return wood; }
+			set{ wood = value; }
+		}
 
-        [SerializeField]
-        private int woodIncome = 2;
+		[SerializeField]
+		private int woodIncome = 2;
 
-        public int WoodIncome
-        {
-            get { return woodIncome; }
-            set{ woodIncome = value; }
-        }
+		public int WoodIncome {
+			get { return woodIncome; }
+			set{ woodIncome = value; }
+		}
 
-        [SerializeField]
-        private float woodIncomeDelay = 5f;
-        private float woodIncomeTimer = 0f;
+		[SerializeField]
+		private float woodIncomeDelay = 5f;
+		private float woodIncomeTimer = 0f;
 
-        private int supply = 0;
+		public float WoodIncomeDelay {
+			get {
+				return this.woodIncomeDelay;
+			}
+		}
 
-        public int Supply
-        {
-            get{ return supply; }
-            set{ supply = value; }
-        }
+		private int supply = 0;
 
-        [SerializeField]
-        private int supplyMax = 10;
+		public int Supply {
+			get{ return supply; }
+			set{ supply = value; }
+		}
 
-        public int SupplyMax
-        {
-            get{ return supplyMax; }
-            set{ supplyMax = value; }
-        }
+		[SerializeField]
+		private int supplyMax = 10;
 
-        private Text goldText = null;
-        private Text goldIncomeText = null;
-        private Text woodText = null;
-        private Text woodIncomeText = null;
-        private Text supplyText = null;
+		public int SupplyMax {
+			get{ return supplyMax; }
+			set{ supplyMax = value; }
+		}
 
-        void Update()
-        {
-            woodIncomeTimer += Time.deltaTime;
-            if(woodIncomeTimer >= woodIncomeDelay)
-            {
-                woodIncomeTimer = 0;
-                wood += woodIncome;
-            }
-            UpdateInfo();
-        }
+		private Text goldText = null;
+		private Text goldIncomeText = null;
+		private Text woodText = null;
+		private Text woodIncomeText = null;
+		private Text supplyText = null;
 
-        void UpdateInfo()
-        {
-            if(goldText == null)
-                goldText = transform.Find("Gold/Gold(Text)").GetComponent<Text>();
-            goldText.text = gold.ToString();
+		void Update()
+		{
+			woodIncomeTimer += Time.deltaTime;
+			if (woodIncomeTimer >= woodIncomeDelay)
+			{
+				woodIncomeTimer = 0;
+				wood += woodIncome;
+			}
+			UpdateInfo();
+		}
 
-            if(goldIncomeText == null)
-                goldIncomeText = transform.Find("GoldIncome/GoldIncome(Text)").GetComponent<Text>();
-            goldIncomeText.text = goldIncome + "/w";
+		void UpdateInfo()
+		{
+			if (goldText == null)
+				goldText = transform.Find("Gold/Gold(Text)").GetComponent<Text>();
+			goldText.text = gold.ToString();
 
-            if(woodText == null)
-                woodText = transform.Find("Wood/Wood(Text)").GetComponent<Text>();
-            woodText.text = wood.ToString();
+			if (goldIncomeText == null)
+				goldIncomeText = transform.Find("GoldIncome/GoldIncome(Text)").GetComponent<Text>();
+			goldIncomeText.text = goldIncome + "/w";
 
-            if(woodIncomeText == null)
-                woodIncomeText = transform.Find("WoodIncome/Income(Text)").GetComponent<Text>();
-            woodIncomeText.text = woodIncome.ToString();
+			if (woodText == null)
+				woodText = transform.Find("Wood/Wood(Text)").GetComponent<Text>();
+			woodText.text = wood.ToString();
 
-            if(supplyText == null)
-                supplyText = transform.Find("Supply/Supply(Text)").GetComponent<Text>();
-            supplyText.text = supply + "/" + supplyMax;
-        }
+			if (woodIncomeText == null)
+				woodIncomeText = transform.Find("WoodIncome/Income(Text)").GetComponent<Text>();
+			woodIncomeText.text = woodIncome.ToString();
 
-        public bool TryPayingGold(int gold)
-        {
-            if(gold > this.gold)
-                return false;
-            this.gold -= gold;
-            return true;
-        }
+			if (supplyText == null)
+				supplyText = transform.Find("Supply/Supply(Text)").GetComponent<Text>();
+			supplyText.text = supply + "/" + supplyMax;
+		}
 
-        public bool TryPayingWood(int wood)
-        {
-            if(wood > this.wood)
-                return false;
-            this.wood -= wood;
-            return true;
-        }
+		public bool TryPayingGold(int gold)
+		{
+			if (gold > this.gold)
+				return false;
+			this.gold -= gold;
+			return true;
+		}
 
-        public bool TryPaying(int gold, int wood)
-        {
-            if(gold > this.gold || wood > this.wood)
-                return false;
-            this.gold -= gold;
-            this.wood -= wood;
-            return true;
-        }
+		public bool TryPayingWood(int wood)
+		{
+			if (wood > this.wood)
+				return false;
+			this.wood -= wood;
+			return true;
+		}
 
-        public void ApplyGoldIncome()
-        {
-            gold += goldIncome;
-        }
-    }
+		public bool TryPaying(int gold, int wood)
+		{
+			if (gold > this.gold || wood > this.wood)
+				return false;
+			this.gold -= gold;
+			this.wood -= wood;
+			return true;
+		}
+
+		public void ApplyGoldIncome()
+		{
+			gold += goldIncome;
+		}
+	}
 }
