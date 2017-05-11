30acac8541f1e01160849ed773e4047a055b4b66
Alexander Krantz <klako@hotmail.se>
2017-05-11 13:43:40 +0200

    Added ticked healing AOE

---
+++ Assets/Scripts/Game/Units/Spells/Passives/HotTickPassive.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Units.Spells.Passives
+{
+	public class HotTickPassive:TickPassive
+	{
+		[SerializeField]
+		[Tooltip("Leave as zero if all friendlys should be healed")]
+		private int maxFriendlies = 2;
+
+		[SerializeField]
+		private float heal = 10f;
+
+		public int MaxFriendlies {
+			get {
+				return this.maxFriendlies;
+			}
+			set {
+				maxFriendlies = value;
+			}
+		}
+
+		public float Heal {
+			get {
+				return this.heal;
+			}
+			set {
+				heal = value;
+			}
+		}
+
+		protected override void ApplyTick(float deltaTime)
+		{
+			List<UnitBehaviour> friendlies = new List<UnitBehaviour>(owner.GetFriendlies());
+			for (int i = 0; i < maxFriendlies; i++)
+			{
+				UnitBehaviour target = friendlies[UnityEngine.Random.Range(0, friendlies.Count)];
+				friendlies.Remove(target);
+
+				target.ApplyHeal(heal, owner);
+			}
+		}
+	}
+}
+
---
+++ Assets/Scripts/Game/Units/Spells/Passives/TickPassive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Units.Spells.Passives
+{
+	public abstract class TickPassive:Passive
+	{
+		[Header("Spell data")]
+
+		[SerializeField]
+		private float tickTime = 1f;
+
+		public float TickTime {
+			get {
+				return this.tickTime;
+			}
+			set {
+				tickTime = value;
+			}
+		}
+
+		private float tickTimer = 0f;
+
+		protected override void Update()
+		{
+			base.Update();
+			tickTimer += Time.deltaTime;
+			if (tickTimer >= tickTime)
+			{
+				ApplyTick(tickTime == 0f ? tickTimer : tickTime);
+				tickTimer = 0f;
+			}
+		}
+
+		protected abstract void ApplyTick(float deltaTime);
+	}
+}
+
