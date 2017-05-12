d53c1d9a02ab35d9ce77b697a29a5f429d0f3967
vakeneller <vakeneller@gmail.com>
2017-05-12 23:36:41 +0200

    Fixed so you can return to the game from the loosing screen

---
+++ Assets/Graphics/Map/Map parts/ripstonebehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ripstonebehaviour : MonoBehaviour {
+	
+	public void onClick () 
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			SceneManager.LoadScene(0);
+		}
+	}
+}
