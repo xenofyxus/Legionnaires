6ffae617346f8a5fe1ca4900410d7e51f237af9b
VirreCarre <victor.carle@gmail.com>
2017-05-15 11:51:33 +0200

    fixed settings and pause

---
+++ Assets/Scripts/Game/Interface/PauseGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void Pause(){
+		Time.timeScale = 0;
+	}
+
+	public void UnPause(){
+		Time.timeScale = 1;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
