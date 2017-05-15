53477fe1383dc599939e1fc9b56dd82ceeb82b55
Daniel <b.daniel.karlsson@gmail.com>
2017-05-15 13:01:48 +0200

    Added legionnaire chooser in settings

--- Assets/Scripts/Menu/SettingsPanelBehaviour.cs
+++ Assets/Scripts/Menu/SettingsPanelBehaviour.cs
@@ -2,29 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 namespace Menu
 {
 	public class SettingsPanelBehaviour : MonoBehaviour
 	{
 		Toggle tooltipBarEnabledToggle = null;
+		ToggleGroup legionnaireToggle = null;
 
-		// Use this for initialization
 		void Start()
 		{
 			tooltipBarEnabledToggle = transform.Find("TooltipBarEnabled").GetComponent<Toggle>();
 			tooltipBarEnabledToggle.isOn = Settings.Current.TooltipBarEnabled;
+
+			legionnaireToggle = transform.Find ("LegionnaireToggle").GetComponent<ToggleGroup> ();
 		}
 	
-		// Update is called once per frame
 		void Update()
 		{
-		
+
 		}
 
 		public void ToggleTooltipBarEnabled()
 		{
 			Settings.Current.TooltipBarEnabled = tooltipBarEnabledToggle.isOn;
+		}
+		public void ToggleLegionnaire()
+		{
+			if (legionnaireToggle.ActiveToggles ().First ().gameObject.name == "OrcEnabled") {
+				Settings.Current.LegionnaireBuilder = LegionnaireBuilder.Orc;
+			} else {
+				Settings.Current.LegionnaireBuilder = LegionnaireBuilder.Human;
+			}
+			print (Settings.Current.LegionnaireBuilder);
 		}
 	}
 }
