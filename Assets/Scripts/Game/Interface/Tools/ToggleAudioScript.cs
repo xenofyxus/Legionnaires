51bbb367a755631a3f733b9562c2e71a71eb8eaf
Daniel <b.daniel.karlsson@gmail.com>
2017-05-16 15:18:18 +0200

    Audio enable/disable works in both gamescene and menuscene

---
+++ Assets/Scripts/Game/Interface/Tools/ToggleAudioScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu{
+public class ToggleAudioScript : MonoBehaviour {
+
+	Toggle audioToggle = null;
+
+	public void Start()
+	{
+			audioToggle = transform.Find ("Audio(Button)").GetComponent<Toggle> ();
+			audioToggle.isOn = !Settings.Current.AudioEnabled;
+	}
+	public void ToggleAudio(){
+
+		Settings.Current.AudioEnabled = !audioToggle.isOn;
+	}
+	}
+		
+}
