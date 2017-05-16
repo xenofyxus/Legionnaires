d5cdb9ab72e37a83d890f72584bd2a3fa36f0be6
Daniel <b.daniel.karlsson@gmail.com>
2017-05-16 21:08:25 +0200

    fixed scaling on loosingscreen and added message depending on score

--- Assets/Scripts/Game/Interface/Tools/ShowScore.cs
+++ Assets/Scripts/Game/Interface/Tools/ShowScore.cs
@@ -7,10 +7,31 @@ public class ShowScore : MonoBehaviour {
 
 	Text scoreText;
 	Text headLine;
+	Text messageText;
+	string message = "You let the king down";
 
 	void Start () {
 		headLine = GameObject.Find ("Description").GetComponent<Text> ();
 		scoreText = transform.GetComponent<Text> ();
+		messageText = GameObject.Find ("Message").GetComponent<Text> ();
+
+		if(PlayerPrefs.GetInt("LatestScore", 0) > 2000){
+			message = "Not too shabby";
+			if(PlayerPrefs.GetInt("LatestScore", 0) > 5000){
+				message = "Well done";
+				if(PlayerPrefs.GetInt("LatestScore", 0) > 10000){
+					message = "You play too much";
+					if(PlayerPrefs.GetInt("LatestScore", 0) > 15000){
+						message = "Are you cheating?";
+						if(PlayerPrefs.GetInt("LatestScore", 0) > 25000){
+							message = "Got nothing more to say";
+						}
+					}
+				}
+			}
+
+		}
+		messageText.text = message;
 		scoreText.text = string.Format("YOUR SCORE:" + '\n' + PlayerPrefs.GetInt("LatestScore", 0));
 	}
 	void Update(){
