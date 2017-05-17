2570df446a839249d5bd994a452fd1edb99a8927
Daniel <b.daniel.karlsson@gmail.com>
2017-05-17 09:10:37 +0200

    changed message on loosingscreen so it is possible to reach

--- Assets/Scripts/Game/Interface/Tools/ShowScore.cs
+++ Assets/Scripts/Game/Interface/Tools/ShowScore.cs
@@ -15,13 +15,13 @@ public class ShowScore : MonoBehaviour {
 		scoreText = transform.GetComponent<Text> ();
 		messageText = GameObject.Find ("Message").GetComponent<Text> ();
 
-		if(PlayerPrefs.GetInt("LatestScore", 0) > 2000){
+		if(PlayerPrefs.GetInt("LatestScore", 0) > 1000){
 			message = "Not too shabby";
-			if(PlayerPrefs.GetInt("LatestScore", 0) > 5000){
+			if(PlayerPrefs.GetInt("LatestScore", 0) > 2500){
 				message = "Well done";
-				if(PlayerPrefs.GetInt("LatestScore", 0) > 10000){
+				if(PlayerPrefs.GetInt("LatestScore", 0) > 5000){
 					message = "You play too much";
-					if(PlayerPrefs.GetInt("LatestScore", 0) > 15000){
+					if(PlayerPrefs.GetInt("LatestScore", 0) > 10000){
 						message = "Are you cheating?";
 						if(PlayerPrefs.GetInt("LatestScore", 0) > 25000){
 							message = "Got nothing more to say";
