fa264001a6184281dd2873187c2485f84c557154
vakeneller <vakeneller@gmail.com>
2017-06-01 13:03:21 +0200

    Fixed small things and experiemented with timescale a bit

---
+++ Assets/Scripts/thisistest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thisistest : MonoBehaviour {
+
+	// Use this for initialization
+	public void OnFuckingClick () {
+		if (Time.timeScale == 1.0F)
+			Time.timeScale = 1.5F;
+		else if (Time.timeScale == 1.5F)
+			Time.timeScale = 5.0F;
+		else
+			Time.timeScale = 1.0F;
+	}
+}
